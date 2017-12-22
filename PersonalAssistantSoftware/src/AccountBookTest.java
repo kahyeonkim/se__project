669b2be4a5232f2e3b09f778aef1d2a1e749ad3e
soobeeny <99xiubin@gamil.com>
2017-12-22 15:18:37 +0900

    test

---
+++ PersonalAssistantSoftware/src/AccountBookTest.java
@@ -0,0 +1,20 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class AccountBookTest {
+
+	@Test
+	public void testCreateAccount(){
+		
+	}
+	
+	@Test
+	public void testUpdateAccount(){
+		
+	}
+	
+	@Test
+	public void testDeleteAccount(){
+		
+	}
+}
